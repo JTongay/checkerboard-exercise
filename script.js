fd80eee3fa9da1fd8e146b01c402eb8cc81ca052
Joseph Tongay <dirk.tongay@gmail.com>
2016-08-24 21:18:41 -0500

    base checkerboard yo

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your JS goes here
+(function(doc) {
+
+    var body = doc.getElementsByTagName('body')[0];
+    var wrapper = doc.createElement('div');
+
+    for (var i = 0; i < 63; i++) {
+        var colors = doc.createElement('div');
+
+        colors.style.float = "left"
+        colors.style.width = "11.1%"
+        colors.style.paddingBottom = "11.1%"
+
+        if (i % 2 === 0) {
+            colors.style.backgroundColor = "red"
+        } else {
+            colors.style.backgroundColor = "black"
+        }
+        wrapper.appendChild(colors)
+        body.appendChild(wrapper);
+    }
+
+
+
+
+
+})(document)
