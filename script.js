b1e760015d41a6a61b4d97596cd7cb9178724c4d
Joseph Tongay <dirk.tongay@gmail.com>
2016-08-24 21:32:41 -0500

    random colors yo

--- script.js
+++ script.js
@@ -10,12 +10,13 @@
         colors.style.float = "left"
         colors.style.width = "11.1%"
         colors.style.paddingBottom = "11.1%"
+        colors.style.backgroundColor = "#" + (Math.floor(Math.random() * 1000)).toString()
 
-        if (i % 2 === 0) {
-            colors.style.backgroundColor = "red"
-        } else {
-            colors.style.backgroundColor = "black"
-        }
+        // if (i % 2 === 0) {
+        //     colors.style.backgroundColor = "red"
+        // } else {
+        //     colors.style.backgroundColor = "black"
+        // }
         wrapper.appendChild(colors)
         body.appendChild(wrapper);
     }
