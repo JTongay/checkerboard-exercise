df6789d28c584ff5b14acebccea3b5fea348a52a
Joseph Tongay <dirk.tongay@gmail.com>
2016-08-27 22:24:30 -0500

    flashing colors yo

--- script.js
+++ script.js
@@ -4,23 +4,44 @@
     var body = doc.getElementsByTagName('body')[0];
     var wrapper = doc.createElement('div');
 
-    for (var i = 0; i < 63; i++) {
-        var colors = doc.createElement('div');
 
+    function createBoard() {
+        var colors = doc.createElement('div');
         colors.style.float = "left"
         colors.style.width = "11.1%"
         colors.style.paddingBottom = "11.1%"
-        colors.style.backgroundColor = "#" + (Math.floor(Math.random() * 1000)).toString()
+        colors.className = "colors"
+        colors.style.backgroundColor = randomColor();
 
 
-        // if (i % 2 === 0) {
-        //     colors.style.backgroundColor = "red"
-        // } else {
-        //     colors.style.backgroundColor = "black"
-        // }
+
         wrapper.appendChild(colors)
         body.appendChild(wrapper);
     }
+
+    for (var i = 0; i < 63; i++) {
+
+        createBoard()
+    }
+
+    function randomColor() {
+        return "#" + (Math.floor(Math.random() * 1000)).toString()
+    }
+
+    setInterval(function() {
+        var getColors = doc.getElementsByClassName("colors");
+        for (var j = 0; j < getColors.length; j++) {
+            getColors[j].style.backgroundColor = randomColor();
+        }
+    }, 2000)
+
+
+
+    // function newBoard() {
+    //     setTimeout(randomColor, 2000)
+    // }
+
+
 
 
 
